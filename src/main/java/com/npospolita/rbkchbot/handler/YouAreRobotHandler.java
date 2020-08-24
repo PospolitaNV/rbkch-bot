e611c347a88a07aa83757f48cb84c1f9e9b37dc5
NPospolita <nikita7asics@gmail.com>
2020-08-24 14:53:56 +0300

    Added "ты робот?" bikanyl question

---
+++ src/main/java/com/npospolita/rbkchbot/handler/YouAreRobotHandler.java
@@ -0,0 +1,29 @@
+package com.npospolita.rbkchbot.handler;
+
+import com.npospolita.rbkchbot.api.TelegramApi;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+@RequiredArgsConstructor
+public class YouAreRobotHandler implements Handler {
+
+    private final TelegramApi api;
+
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    @Override
+    public boolean canHandle(Update update) {
+        return !update.message().from().isBot() && Double.compare(random.nextDouble(), 0.003) < 0;
+    }
+
+    @Override
+    public void handle(Update update) {
+        api.sendMessage(update, "ты робот?");
+    }
+
+}
