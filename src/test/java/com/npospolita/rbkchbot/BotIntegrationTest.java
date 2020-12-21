60a6f3b60a8fb1ae9b56372a683be27dfc072d54
NPospolita <nikita7asics@gmail.com>
2020-12-21 23:26:08 +0300

    enabled test

--- src/test/java/com/npospolita/rbkchbot/BotIntegrationTest.java
+++ src/test/java/com/npospolita/rbkchbot/BotIntegrationTest.java
@@ -4,7 +4,6 @@ import com.npospolita.rbkchbot.repo.WorkingChatRepository;
 import com.npospolita.rbkchbot.service.ChatService;
 import com.npospolita.rbkchbot.service.UpdateProcessService;
 import com.pengrad.telegrambot.model.Update;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
@@ -12,7 +11,6 @@ import org.springframework.cache.CacheManager;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled
 class BotIntegrationTest extends DatabaseTestBase {
 
     @Autowired
