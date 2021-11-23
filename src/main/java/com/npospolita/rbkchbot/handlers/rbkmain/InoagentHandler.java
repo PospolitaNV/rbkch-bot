cd9d39044a05bd2b81e0be7ccfc7a4799d4bd087
inallyoung <in.all.young@gmail.com>
2021-11-23 18:26:46 +0300

    Added inoagent handler

---
+++ src/main/java/com/npospolita/rbkchbot/handlers/rbkmain/InoagentHandler.java
@@ -0,0 +1,32 @@
+package com.npospolita.rbkchbot.handlers.rbkmain;
+
+import com.npospolita.rbkchbot.api.TelegramApi;
+import com.npospolita.rbkchbot.handlers.CommonTextMessageHandler;
+import com.npospolita.rbkchbot.handlers.Result;
+import com.pengrad.telegrambot.model.Update;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class InoagentHandler extends CommonTextMessageHandler {
+
+    private final TelegramApi telegramApi;
+
+    @Value("${inoagent.users}")
+    private List<Integer> inoagents;
+
+    @Value("${inoagent.text}")
+    private String inoagentText;
+
+    @Override
+    public Result handle(Update update) {
+        if (inoagents.contains(update.message().from().id())) {
+            telegramApi.sendMessage(update, inoagentText);
+        }
+        return Result.STOP;
+    }
+}
